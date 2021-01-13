54128bf52ea8abd1a14da40747f84a90dd6a908a
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-01-13 11:40:22 +0200

    fixing the color code of the buttons

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -1,10 +1,19 @@
 const contactForm = document.querySelector("#contact-form");
 
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const FD = new FormData(contactForm);
-  // Display the values
-  for (var value of FD.values()) {
-    console.log(value);
+contactForm.addEventListener("submit", async (e) => {
+  await e.preventDefault();
+  try {
+    const FD = await new FormData(contactForm);
+    const stringified = await JSON.stringify(Object.fromEntries(FD));
+    let res = await fetch("http://localhost:1000/api/contact/email", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: stringified,
+    });
+    let data = await res.json();
+    console.log(data);
+    contactForm.reset(); 
+  } catch (error) {
+    console.log(error);
   }
 });
